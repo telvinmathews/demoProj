6bde563db8d8a9a8e2153845fe9b667018906ae9
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-10-01 10:13:19 -0500

    inital commit

---
+++ src/main/java/com/example/exmpleproj/ExmpleProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.exmpleproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExmpleProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExmpleProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exmpleproj/ExmpleProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exmpleproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExmpleProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
